cd6320cf2c7159fec9052c31b27d793d21fcd019
jihyunni <113456378+jihyunni@users.noreply.github.com>
2024-06-07 02:42:16 +0900

    Update Real_total.py

--- Real_total.py
+++ Real_total.py
@@ -104,18 +104,6 @@ def measurements (f_, f):
     vol_f = cnt_f * vol
     ccvol_b = vol_b * 0.001
     ccvol_f = vol_f * 0.001
-
-#     voxel_size = f.header.get_zooms()
-#     voxel_volume = np.prod(voxel_size)
-
-#     # 각 레이블의 부피를 계산합니다.
-#     unique_values, counts = np.unique(f_, return_counts=True)
-#     volumes = {value: count * voxel_volume for value, count in zip(unique_values, counts)}
-#     volumes_cc = {value: volume / 1000 for value, volume in volumes.items()}
-
-#     # 레이블 1 (유방 부피)와 레이블 2 (FGT 부피)를 찾고 부피를 계산합니다.
-#     ccvol_b = volumes_cc.get(1, 0.0)
-#     ccvol_f = volumes_cc.get(2, 0.0)
     
     col2.write("### [Measurements]")
     col2.write(f"- Breast volume : {ccvol_b:.2f} (cc)")
@@ -125,7 +113,6 @@ def measurements (f_, f):
     workbook = xlsxwriter.Workbook(output, {'in_memory': True})
     worksheet = workbook.add_worksheet()
     worksheet.write('A1', 'pid')
-#     worksheet.write('A2', '')
     worksheet.write('B1', 'breast')
     worksheet.write('B2', ccvol_b)
     worksheet.write('C1', 'fgt')
@@ -138,61 +125,22 @@ def measurements (f_, f):
             file_name="measurement.xlsx",
             mime="application/vnd.ms-excel"
         )
-        
-# def measurements (f_, f):
-#     margin = 10
-#     breast_region = np.where(f_ == 1)
-#     fgt_region = np.where(f_ == 2)
 
-#     if len(breast_region[0]) == 0 or len(fgt_region[0]) == 0:
-#         raise ValueError("유방 또는 FGT 영역을 찾을 수 없습니다.")
+def removing_FP (mask_):
+    mask_ = np.round(mask_)
+    orig_mask = mask_.copy()
+    mask = nd.binary_closing(mask_,structure=np.ones((1,1,1)), iterations=1)
+    l, m = nd.label(mask_, structure=np.ones((3,3,3)))
+    vols = []
+    for i in range(m+1):
+        vols.append(np.sum(l==i))
+    vind = np.argsort(vols)
+    new_mask = np.zeros(mask_.shape)
+    new_mask[l==vind[-2]]=1
+    res = new_mask * orig_mask
+    res = np.round(res)
+    return res
 
-#     # 유방 영역의 최소 및 최대 좌표 계산
-#     x_min, x_max = breast_region[0].min(), breast_region[0].max()
-#     y_min, y_max = breast_region[1].min(), breast_region[1].max()
-#     z_min, z_max = breast_region[2].min(), breast_region[2].max()
-
-#     # 바운딩 박스를 기준으로 일정 범위 외부의 가장자리 제거
-#     f_[0:max(0, x_min - margin), :, :] = 0
-#     f_[min(f_.shape[0], x_max + margin):, :, :] = 0
-#     f_[:, 0:max(0, y_min - margin), :] = 0
-#     f_[:, min(f_.shape[1], y_max + margin):, :] = 0
-#     f_[:, :, 0:max(0, z_min - margin)] = 0
-#     f_[:, :, min(f_.shape[2], z_max + margin):] = 0
-
-#     # 볼륨 계산
-#     cnt_b = np.sum(f_ == 1)
-#     cnt_f = np.sum(f_ == 2)
-#     sx, sy, sz = f.header.get_zooms()
-#     vol = sx * sy * sz
-#     vol_b = cnt_b * vol
-#     vol_f = cnt_f * vol
-#     ccvol_b = vol_b * 0.001
-#     ccvol_f = vol_f * 0.001
-
-#     # 결과 출력
-#     col2.write("### [Measurements]")
-#     col2.write(f"- Breast volume : {ccvol_b:.2f} (cc)")
-#     col2.write(f"- FGT volume : {ccvol_f:.2f} (cc)")
-
-#     # 엑셀 파일 생성
-#     output = BytesIO()
-#     workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-#     worksheet = workbook.add_worksheet()
-#     worksheet.write('A1', 'pid')
-#     worksheet.write('B1', 'breast')
-#     worksheet.write('B2', ccvol_b)
-#     worksheet.write('C1', 'fgt')
-#     worksheet.write('C2', ccvol_f)
-#     workbook.close()
-
-#     with col2:
-#         st.download_button(
-#             label="Download Excel",
-#             data=output.getvalue(),
-#             file_name="measurement.xlsx",
-#             mime="application/vnd.ms-excel"
-#         )
 
 def visual_mask(msk):
     
@@ -245,10 +193,6 @@ def feedback (f, mask):
         nib.save(mask, os.path.join(mydir, 'mask.nii.gz'))
         
         st.write("Thank you for your feedback!")
-        
- 
-
-
     
 def check ():
     if not (os.path.isdir('Python/2024_CapStone/software/jihyun/update_data/good')):
@@ -258,7 +202,6 @@ def check ():
     if (len(original) >= 30):
         return 1
     else: return 0
-
 
 
 col1,col2 = st.columns(2)
@@ -273,29 +216,24 @@ if my_upload1:
     measurements(mask, h)
     visual_mask(mask)
     feedback(h, mask)
-#     processed_npmsk = removing_FP(mask)
+    processed_npmsk = removing_FP(mask)
     
-#     val = check()
+    val = check()
     
+    try:
+         result = subprocess.run(['python3', 'Python/2024_CapStone/software/jihyun/update_code.py'], check=True, capture_output=True, text=True)
+        
+         result = os.system(['python3',os.system('update_code.py')
+         print("Script output:", result.stdout)
+        
+     except subprocess.CalledProcessError as e:
+         print("Error executing script:", e.stderr)
+                             
 #     try:
-#         result = os.system('python3 Python/2024_CapStone/software/jihyun/update_open_code.py')
+#        result = os.system('python3 Python/2024_CapStone/software/jihyun/update_open_code.py')
 #         print("Script executed with exit code:", result)
 #     except Exception as e:
 #         print("An error occurred:", e)
-        
-#     try:
-#         result = subprocess.run(['python3', 'Python/2024_CapStone/software/jihyun/update_code.py'], check=True, capture_output=True, text=True)
-        
-#         result = os.system(['python3',os.system('update_code.py')
-#         print("Script output:", result.stdout)
-        
-#     except subprocess.CalledProcessError as e:
-#         print("Error executing script:", e.stderr)
-
-
-
-#     if (val == 1):
-#         os.system('Python/2024_CapStone/software/jihyun/Untitled-Copy1.ipynb')
 
 
 elif my_upload2:
@@ -305,23 +243,19 @@ elif my_upload2:
     measurements(mask, h)
     visual_mask(mask)
     feedback(h, mask)
-#     processed_npmsk = removing_FP(mask)
-    
+    processed_npmsk = removing_FP(mask)        
+        
+     try:
+         result = subprocess.run(['python3', 'Python/2024_CapStone/software/jihyun/update_code.py'], check=True, capture_output=True, text=True)
+         print("Script output:", result.stdout)
+        
+     except subprocess.CalledProcessError as e:
+         print("Error executing script:", e.stderr)
+
+
 #     val = check()
 #     try:
 #         result = os.system('python3 Python/2024_CapStone/software/jihyun/update_open_code.py')
 #         print("Script executed with exit code:", result)
 #     except Exception as e:
 #         print("An error occurred:", e)
-        
-        
-        
-        
-#     try:
-#         result = subprocess.run(['python3', 'Python/2024_CapStone/software/jihyun/update_code.py'], check=True, capture_output=True, text=True)
-#         print("Script output:", result.stdout)
-        
-#     except subprocess.CalledProcessError as e:
-#         print("Error executing script:", e.stderr)
-
-
